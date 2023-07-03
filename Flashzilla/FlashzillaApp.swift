10f8a84e8706fd13d051cd1819808208380028ca
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-07-03 19:54:43 +0530

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Rishav Gupta on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
